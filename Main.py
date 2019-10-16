21da95964d51a83e81ab22b76349f6ce6791b2ef
star <fatma>
2019-10-16 14:11:36 +0300

    i have bulid the login \sign up interface

--- Main.py
+++ Main.py
@@ -1,16 +1,14 @@
-# import modules
-
 from tkinter import *
 import os
 
 
-# Designing window for login
+# Designing window for registration
 
-def Login():
-    global Login_screen
-    Login_screen = Toplevel(main_screen)
-    Login_screen.title("Login")
-    Login_screen.geometry("300x250")
+def register():
+    global register_screen
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("300x250")
 
     global username
     global password
@@ -19,30 +17,132 @@ def Login():
     username = StringVar()
     password = StringVar()
 
-    Label(Login_screen, text="Please enter details below", bg="blue").pack()
-    Label(Login_screen, text="").pack()
-    username_lable = Label(Login_screen, text="Username * ")
+    Label(register_screen, text="Please enter details below", bg="red").pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username * ")
     username_lable.pack()
-    username_entry = Entry(Login_screen, textvariable=username)
+    username_entry = Entry(register_screen, textvariable=username)
     username_entry.pack()
-    password_lable = Label(Login_screen, text="Password * ")
+    password_lable = Label(register_screen, text="Password * ")
     password_lable.pack()
-    password_entry = Entry(Login_screen, textvariable=password, show='*')
+    password_entry = Entry(register_screen, textvariable=password, show='*')
     password_entry.pack()
-    Label(Login_screen, text="").pack()
-    Button(Login_screen, text="Login", width=10, height=1, bg="blue", command=Login_user).pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, bg="blue", command=register_user).pack()
+
+
+# Designing window for login
+
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_login_entry
+    global password_login_entry
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
+
+
+# Implementing event on register button
+
+def register_user():
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info+".txt", "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
 
 
 # Implementing event on login button
 
-def Login_user():
-    username_info = username.get()
-    password_info = password.get()
-    print(username_info)
-    print(password_info)
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1+".txt", "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+        else:
+            password_not_recognised()
+    else:
+        user_not_found()
 
 
-    Label(Login_screen, text="Login Success", fg="green", font=("calibri", 11)).pack()
+# Designing popup for login success
+
+def login_sucess():
+    global login_success_screen
+    login_success_screen = Toplevel(login_screen)
+    login_success_screen.title("Success")
+    login_success_screen.geometry("150x100")
+    Label(login_success_screen, text="Login Success").pack()
+    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+
+
+# Designing popup for login invalid password
+
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+
+
+# Designing popup for user not found
+
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+
+
+# Deleting popups
+
+def delete_login_success():
+    login_success_screen.destroy()
+
+
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+
+
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
 
 
 # Designing Main(first) window
@@ -54,9 +154,9 @@ def main_account_screen():
     main_screen.title("Account Login")
     Label(text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
     Label(text="").pack()
-    # Button(text="Login", height="2", width="30", command=login).pack()
+    Button(text="Login", height="2", width="30", command=login).pack()
     Label(text="").pack()
-    Button(text="Login", height="2", width="30", command=Login).pack()
+    Button(text="Register", height="2", width="30", command=register).pack()
 
     main_screen.mainloop()
 
