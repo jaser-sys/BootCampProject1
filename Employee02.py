35fb485ae11ea133b1d51b3c7b6b257835bc1407
aminaltory <aminaltory@outlook.com>
2019-10-16 09:06:25 +0300

    first upload

---
+++ Employee02.py
@@ -0,0 +1,92 @@
+import Employee
+
+
+def emp( ):
+
+    emp1 = Employee.Employee('name', 'id', 'department', 'title')
+    emp2 = Employee.Employee('name', 'id', 'department', 'title')
+    emp3 = Employee.Employee('name', 'id', 'department', 'title')
+    emp4 = Employee.Employee('name', 'id', 'department', 'title')
+    emp5 = Employee.Employee('name', 'id', 'department', 'title')
+    emp6 = Employee.Employee('name', 'id', 'department', 'title')
+    emp7 = Employee.Employee('name', 'id', 'department', 'title')
+    emp8 = Employee.Employee('name', 'id', 'department', 'title')
+    emp9 = Employee.Employee('name', 'id', 'department', 'title')
+    emp10 = Employee.Employee('name', 'id', 'department', 'title')
+
+
+    emp1.set_name('Susan Meyers')
+    emp1.set_id('001')
+    emp1.set_department('Accounting')
+    emp1.set_title('Vice President')
+
+    emp2.set_name('Mark Jones')
+    emp2.set_id('002')
+    emp2.set_department('IT')
+    emp2.set_title('Programmer')
+
+    emp3.set_name('Joy Rogersr')
+    emp3.set_id('003')
+    emp3.set_department('Manufacturing')
+    emp3.set_title('Engineer')
+
+    emp4.set_name('Joy Rogersr')
+    emp4.set_id('004')
+    emp4.set_department('Manufacturing')
+    emp4.set_title('Engineer')
+
+    emp5.set_name('Joy Rogersr')
+    emp5.set_id('005')
+    emp5.set_department('Manufacturing')
+    emp5.set_title('Engineer')
+
+    emp6.set_name('Joy Rogersr')
+    emp6.set_id('006')
+    emp6.set_department('Manufacturing')
+    emp6.set_title('Engineer')
+
+    emp7.set_name('Joy Rogersr')
+    emp7.set_id('007')
+    emp7.set_department('Manufacturing')
+    emp7.set_title('Engineer')
+
+    emp8.set_name('Joy Rogersr')
+    emp8.set_id('008')
+    emp8.set_department('Manufacturing')
+    emp8.set_title('Engineer')
+
+    emp9.set_name('Joy Rogersr')
+    emp9.set_id('009')
+    emp9.set_department('Manufacturing')
+    emp9.set_title('Engineer')
+
+    emp10.set_name('Joy Rogersr')
+    emp10.set_id('010')
+    emp10.set_department('Manufacturing')
+    emp10.set_title('Engineer')
+
+
+
+    print()
+    print(emp1)
+    print()
+    print(emp2)
+    print()
+    print(emp3)
+    print()
+    print(emp4)
+    print()
+    print(emp5)
+    print()
+    print(emp6)
+    print()
+    print(emp7)
+    print()
+    print(emp8)
+    print()
+    print(emp9)
+    print()
+    print(emp10)
+
+
+emp( )
