e5f94798929d17638987ecf1df6ae0e4fd9dafe9
star <fatma>
2019-10-16 14:29:05 +0300

    now you have a working login\signup interface

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 from tkinter import *
 import os
 
-
-# Designing window for registration
-
+#####################################
+# Designing window for registration #
+#####################################
 def register():
     global register_screen
     register_screen = Toplevel(main_screen)
@@ -30,9 +30,9 @@ def register():
     Label(register_screen, text="").pack()
     Button(register_screen, text="Register", width=10, height=1, bg="blue", command=register_user).pack()
 
-
-# Designing window for login
-
+#############################
+# Designing window for login#
+#############################
 def login():
     global login_screen
     login_screen = Toplevel(main_screen)
@@ -60,14 +60,14 @@ def login():
     Label(login_screen, text="").pack()
     Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
 
-
-# Implementing event on register button
-
+##################################################
+# When Clicking on event thath is register button#
+##################################################
 def register_user():
     username_info = username.get()
     password_info = password.get()
 
-    file = open(username_info+".txt", "w")
+    file = open(username_info, "w")
     file.write(username_info + "\n")
     file.write(password_info)
     file.close()
@@ -77,9 +77,9 @@ def register_user():
 
     Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
 
-
-# Implementing event on login button
-
+#####################################
+# When Clicking on event thath is login button#
+#####################################
 def login_verify():
     username1 = username_verify.get()
     password1 = password_verify.get()
@@ -88,7 +88,7 @@ def login_verify():
 
     list_of_files = os.listdir()
     if username1 in list_of_files:
-        file1 = open(username1+".txt", "r")
+        file1 = open(username1, "r")
         verify = file1.read().splitlines()
         if password1 in verify:
             login_sucess()
@@ -97,9 +97,9 @@ def login_verify():
     else:
         user_not_found()
 
-
-# Designing popup for login success
-
+####################################
+# Designing popup for login success#
+####################################
 def login_sucess():
     global login_success_screen
     login_success_screen = Toplevel(login_screen)
@@ -108,9 +108,9 @@ def login_sucess():
     Label(login_success_screen, text="Login Success").pack()
     Button(login_success_screen, text="OK", command=delete_login_success).pack()
 
-
-# Designing popup for login invalid password
-
+#############################################
+# Designing popup for login invalid password#
+#############################################
 def password_not_recognised():
     global password_not_recog_screen
     password_not_recog_screen = Toplevel(login_screen)
@@ -119,9 +119,9 @@ def password_not_recognised():
     Label(password_not_recog_screen, text="Invalid Password ").pack()
     Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
 
-
-# Designing popup for user not found
-
+#####################################
+# Designing popup for user not found#
+#####################################
 def user_not_found():
     global user_not_found_screen
     user_not_found_screen = Toplevel(login_screen)
@@ -130,9 +130,9 @@ def user_not_found():
     Label(user_not_found_screen, text="User Not Found").pack()
     Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
 
-
-# Deleting popups
-
+##################
+# Deleting popups#
+##################
 def delete_login_success():
     login_success_screen.destroy()
 
@@ -144,9 +144,9 @@ def delete_password_not_recognised():
 def delete_user_not_found_screen():
     user_not_found_screen.destroy()
 
-
-# Designing Main(first) window
-
+###############################
+# Designing Main(first) window#
+###############################
 def main_account_screen():
     global main_screen
     main_screen = Tk()
